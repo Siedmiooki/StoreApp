0910a379cecb01cf38e441eb2f92fbb73b0cfa2a
Jan Karpowicz <jan.karpowicz11@gmail.com>
2021-11-25 17:29:36 +0100

    8. nav icons + cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalStyles from "./components/Globalstyles";
 import { Routes, Route } from "react-router-dom";
-import { Navbar, Homepage, WomensPage, MensPage, OutletPage, ItemDetails } from "./components"
+import { Navbar, Homepage, WomensPage, MensPage, OutletPage, ItemDetails, CartPage } from "./components"
 
 
 function App() {
+
+    // cart + add to cart
+
+    const [cartItems, setCartItems] = useState([])
+
+    const addToCartHandler = ({ id, name, price, img }) => {
+        const addedItem = {
+            id: id,
+            name: name,
+            price: price,
+            img: img
+        }
+        setCartItems([...cartItems, addedItem])
+    }
+
+
     return (
         <div className="app">
             <GlobalStyles />
@@ -18,7 +34,8 @@ function App() {
                         <Route exact path="/women" element={<WomensPage />} />
                         <Route exact path="/men" element={<MensPage />} />
                         <Route exact path="/outlet" element={<OutletPage />} />
-                        <Route exact path="/:id" element={<ItemDetails />} />
+                        <Route exact path="/:id" element={<ItemDetails addToCartHandler={addToCartHandler} />} />
+                        <Route exact path="/cart" element={<CartPage cartItems={cartItems} />} />
                     </Routes>
                 </div>
             </div>
